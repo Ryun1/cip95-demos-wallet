62708f5838c1857f609e79ab4c1e43ee7ebd2c6b
Ryan Williams <rwilliams1@firstderivatives.com>
2023-08-09 13:30:45 +0100

    added Demos source code

---
+++ src/pages/Content/injected.js
@@ -0,0 +1,90 @@
+import {
+  enable,
+  getAddress,
+  getBalance,
+  getCollateral,
+  getNetworkId,
+  getRewardAddress,
+  getUtxos,
+  isEnabled,
+  off,
+  on,
+  signData,
+  signDataCIP30,
+  signTx,
+  submitTx,
+  //CIP-95
+  getDRepKey,
+  getStakeKey,
+  signTxCIP95,
+  signDataCIP95,
+} from '../../api/webpage';
+import { EVENT } from '../../config/config';
+
+// CIP-30 + CIP-95
+window.cardano = {
+  ...(window.cardano || {}),
+  demos: {
+    enable: async (obj) => {
+      let extension = undefined;
+      try{
+        extension = obj['cip'];
+      }catch(err) {
+        console.log("Wallet: No extension passed to wallet");
+      }
+
+      if (extension == "95") {
+        if (await enable()) {
+          return {
+            getBalance: () => getBalance(),
+            signData: (address, payload) => signDataCIP30(address, payload),
+            submitTx: (tx) => submitTx(tx),
+            getUtxos: (amount, paginate) => getUtxos(amount, paginate),
+            getUsedAddresses: async () => [await getAddress()],
+            getUnusedAddresses: async () => [],
+            getChangeAddress: () => getAddress(),
+            getRewardAddresses: async () => [await getRewardAddress()],
+            getNetworkId: () => getNetworkId(),
+            experimental: {
+              on: (eventName, callback) => on(eventName, callback),
+              off: (eventName, callback) => off(eventName, callback),
+              getCollateral: () => getCollateral(),
+            },
+            // CIP-95 -----------------------------
+            getPubDRepKey:() => getDRepKey(),
+            getActivePubStakeKeys:() => getStakeKey(),
+            signTx: (tx, partialSign) => signTxCIP95(tx, partialSign),
+            signData: (address, payload) => signDataCIP95(address, payload),
+          };
+        }
+      }  else if (obj == null) {
+        if (await enable()) {
+          return {
+            getBalance: () => getBalance(),
+            signData: (address, payload) => signDataCIP30(address, payload),
+            signTx: (tx, partialSign) => signTx(tx, partialSign),
+            submitTx: (tx) => submitTx(tx),
+            getUtxos: (amount, paginate) => getUtxos(amount, paginate),
+            getUsedAddresses: async () => [await getAddress()],
+            getUnusedAddresses: async () => [],
+            getChangeAddress: () => getAddress(),
+            getRewardAddresses: async () => [await getRewardAddress()],
+            getNetworkId: () => getNetworkId(),
+            experimental: {
+              on: (eventName, callback) => on(eventName, callback),
+              off: (eventName, callback) => off(eventName, callback),
+              getCollateral: () => getCollateral(),
+            },
+          };
+        }
+
+      }
+    },
+    isEnabled: () => isEnabled(),
+    apiVersion: '1.3.1',
+    supportedExtensions: [{'cip': 95}],
+    name: 'demos',
+    icon: "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3Csvg viewBox='0 0 500 500' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='309.36' y='12.441' width='121.115' height='472.347' style='fill: rgb(128  177  211)%3B'/%3E%3Cellipse style='fill: rgb(128  177  211)%3B' cx='231.272' cy='320.966' rx='171.791' ry='137.051'/%3E%3C/svg%3E",
+    _events: {},
+  },
+};
